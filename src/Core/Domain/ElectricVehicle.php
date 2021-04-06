f139e9a1bc8593478ebdc2e05bd5df9672afdc05
Hugo Chinchilla <hugoasecas@gmail.com>
2021-04-06 19:19:58 +0200

    handle left turn from all directions

--- src/Core/Domain/ElectricVehicle.php
+++ src/Core/Domain/ElectricVehicle.php
@@ -6,8 +6,36 @@ namespace Example\App\Core\Domain;
 
 class ElectricVehicle
 {
-    public function execute(string $string): string
+    private string $direction;
+
+    public function __construct()
     {
-        return "0:0:W";
+        $this->direction = "N";
+    }
+
+    public function execute(string $command): string
+    {
+        foreach ($this->parseInstructions($command) as $instruction) {
+            if ($instruction === "L") {
+                $this->direction = $this->rotateLeft();
+            }
+        }
+
+        return "0:0:" . $this->direction;
+    }
+
+    private function rotateLeft(): string
+    {
+        return [
+            'N' => 'W',
+            'W' => 'S',
+            'S' => 'E',
+            'E' => 'N',
+        ][$this->direction];
+    }
+
+    private function parseInstructions(string $command): array
+    {
+        return str_split($command, 1);
     }
 }
--- tests/Core/Domain/ElectricVehicleTest.php
+++ tests/Core/Domain/ElectricVehicleTest.php
@@ -9,13 +9,24 @@ class ElectricVehicleTest extends TestCase
 {
     /**
      * @test
+     * @dataProvider rotateLeftProvider
      */
-    public function a_ev_should_rotate_left(): void
+    public function a_ev_should_rotate_left(string $command, string $expectedOutput): void
     {
         $ev = new ElectricVehicle();
 
-        $result = $ev->execute('L');
+        $result = $ev->execute($command);
 
-        $this->assertEquals('0:0:W', $result);
+        $this->assertEquals($expectedOutput, $result);
+    }
+
+    public function rotateLeftProvider()
+    {
+        return [
+            "If facing north, will face W after one rotation" => ["L", "0:0:W"],
+            "If facing north, will face S after two rotations" => ["LL", "0:0:S"],
+            "If facing north, will face E after three rotations" => ["LLL", "0:0:E"],
+            "If facing north, will face N after four rotations" => ["LLLL", "0:0:N"],
+        ];
     }
 }
