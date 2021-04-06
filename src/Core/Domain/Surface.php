7f199f48343067abaa7d7fdc7309ea25932da6ae
Hugo Chinchilla <hugoasecas@gmail.com>
2021-04-06 20:15:52 +0200

    a surface defines the boundaries to move the EVs

--- src/Core/Domain/Surface.php
+++ src/Core/Domain/Surface.php
@@ -6,7 +6,20 @@ namespace Example\App\Core\Domain;
 
 class Surface
 {
-    public function __construct(private int $width, private int $height)
+    public function __construct(private int $maxX, private int $maxY)
     {
+    }
+
+    public function isValidDestination(Point $point): bool
+    {
+        if ($point->x() > $this->maxX || $point->y() > $this->maxY) {
+            return false;
+        }
+
+        if ($point->x() < 0 || $point->y() < 0) {
+            return false;
+        }
+
+        return true;
     }
 }
--- tests/Core/Domain/SurfaceTest.php
+++ tests/Core/Domain/SurfaceTest.php
@@ -3,19 +3,28 @@
 namespace Example\Tests\Core\Domain;
 
 use Example\App\Core\Domain\ElectricVehicle;
+use Example\App\Core\Domain\Point;
 use Example\App\Core\Domain\Surface;
 use PHPUnit\Framework\TestCase;
 
 class SurfaceTest extends TestCase
 {
     /** @test */
-    public function an_ev_can_move_inside_a_surface(): void
+    public function a_surface_can_reveal_if_a_position_is_valid_to_move_into_it(): void
     {
         $surface = new Surface(10,10);
-        $ev = new ElectricVehicle($surface);
 
-        $ev->execute('M');
+        $this->assertTrue($surface->isValidDestination(new Point(0, 0)));
+    }
 
-        $this->expectNotToPerformAssertions();
+    /** @test */
+    public function the_surface_boundaries_are_invalid_movements(): void
+    {
+        $surface = new Surface(10,10);
+
+        $this->assertFalse($surface->isValidDestination(new Point(11, 10)));
+        $this->assertFalse($surface->isValidDestination(new Point(10, 11)));
+        $this->assertFalse($surface->isValidDestination(new Point(-1, 0)));
+        $this->assertFalse($surface->isValidDestination(new Point(0, -1)));
     }
 }
